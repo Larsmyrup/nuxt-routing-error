849ffd17247776041f8c9a6c23d17c174f389b9d
larsmyrup <hej@larsmyrup.dk>
2023-10-12 11:01:34 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtLink
+      :to="{path: 'routed'}"
+  >
+    LINK
+  </NuxtLink>
+</template>
---
+++ pages/routed.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Routed
+  </div>
+</template>
+
+<script setup lang="ts">
+await new Promise((resolve) => setTimeout(resolve, 10000))
+</script>
+
+<style scoped>
+
+</style>
